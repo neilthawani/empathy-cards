8b0ba5d64da55d014a91dccfa2b18638a28cdef0
neilthawani@gmail.com <neilthawani@gmail.com>
2017-04-25 22:30:08 -0500

    Added ember-hammer for gestures

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -19,6 +19,8 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
+  app.import('bower_components/hammerjs/hammer.js');
+  app.import('bower_components/ember-hammer/ember-hammer.js');
 
   return app.toTree();
 };
